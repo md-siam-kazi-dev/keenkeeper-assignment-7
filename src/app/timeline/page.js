dcef200a9bb39e711442c2ab639ea99543b14425
mdsiamkazi <mdsiamkazibd@gmail.com>
2026-04-15 23:23:32 +0600

    Fix responsibe on timeline

--- src/app/timeline/page.js
+++ src/app/timeline/page.js
@@ -6,7 +6,7 @@ const Timeline = () => {
   
   
   return (
-    <div className="container-div mt-50 sm:mt-30 min-h-[70vh] mb-10">
+    <div className="container-div mt-40 sm:mt-30 min-h-[70vh] mb-10">
       <h1 className="text-5xl font-bold mb-4">Timeline</h1>
         
       <TimlinePage />
